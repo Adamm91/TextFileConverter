a6b10715430b859e0f9a3bd3a610978583612a1a
Adamm91 <adam.marcinkowski91@gmail.com>
2018-04-25 18:24:10 +0200

    first commit

--- src/main/java/pl/sda/CSVFileWriter.java
+++ src/main/java/pl/sda/CSVFileWriter.java
@@ -7,16 +7,6 @@ import java.io.IOException;
 public class CSVFileWriter implements TextFileWriter {
     @Override
     public void write(String filePath) {
-        try {
-            FileWriter fileWriter = new FileWriter(filePath);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            bufferedWriter.write("HELLO");
-
-            bufferedWriter.close();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
